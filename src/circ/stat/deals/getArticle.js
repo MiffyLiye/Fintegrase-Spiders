8dbbfc82301ed47c2835d65c8639968fca570302
Wang, Tao <miffyliye@gmail.com>
2018-07-15 23:18:00 +0800

    update parser

--- src/circ/stat/deals/getArticle.js
+++ src/circ/stat/deals/getArticle.js
@@ -36,12 +36,17 @@ const getArticle = async (uri) => {
   if (rows[0].trim().split(/\n+/).length === 1) {
     data.push({name: '', value: rows[0].trim()});
   }
+  let lastLine;
   for (let i = 1; i < rows.length - 1; i++) {
     const cells = rows[i].trim().split(/\n+/);
     data.push({name: cells[0], value: cells[1]})
+    if (cells[0] === '资产总额') {
+      lastLine = i;
+      break;
+    }
   }
-  const note = rows[rows.length - 1].split('：');
-  data.push({name: note[0], value: note[1].replace(/\n万元$/,'')});
+  const note = _.drop(rows, lastLine + 1).join('\n').replace(/\n+/g,'\n');
+  data.push({name: '备注', value: note.replace(/\n(单位:万元)?(万元)?$/,'')});
 
   return {title, uri, publishedAt, data};
 };
--- src/circ/stat/deals/index.js
+++ src/circ/stat/deals/index.js
@@ -2,11 +2,11 @@ const getList = require('./getList');
 const getArticle = require('./getArticle');
 const request = require('request-promise');
 
-const fintegraseHost = 'http://localhost:7070';
-const authorizationKey = '5678';
+const fintegraseHost = process.env.FINTEGRASE_HOST || 'http://localhost:7070';
+const authorizationKey = process.env.ADMIN_AUTH_TOKEN || '5678';
 
 getList().then(async uris => {
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < uris.length; i++) {
     const document = await getArticle(uris[i]);
     console.log(uris[i], document.title, document.uri);
     const options = {
